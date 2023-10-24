d7652ce34324f6f93ba689d093c79de1cfac49d6
Francisco Andrade <francisco.andrade@tolife.com.br>
2023-10-24 12:06:18 -0300

    bill support

--- source.py
+++ source.py
@@ -1,5 +1,6 @@
 """csv reading and writing tools"""
 import csv
+import math
 
 import os
 import string
@@ -23,13 +24,36 @@ def sort(input_array: Sequence, key: string = "") -> Sequence:
     return
 
 
+def search_legislator_by_id(input_array: Sequence, target: int) -> int:
+    """Find legislator by id, assuming a sorted input sequence"""
+    start = 0
+    end = len(input_array) - 1
+    while start <= end:
+        target_index = (start + end) // 2
+
+        if input_array[target_index][0] == target:
+            return target_index
+
+        elif input_array[target_index][0] > target:
+            end = target_index - 1
+        else:
+            start = target_index + 1
+
+    return -1
+
+
 sorted_legislators: Sequence = []
 sorted_votes_by_legislator_id: Sequence = []
+bills: Sequence = []
+votes: Sequence = []
+map_bill_to_vote = {}
 
 cwd: string = os.getcwd()
 os.chdir(cwd)
 
 cwd = os.getcwd()
+
+# reading
 
 with open(
     f"{cwd}/source" + "\\legislators.csv", newline="", encoding="utf-8"
@@ -73,6 +97,21 @@ with open(
         sorted_votes_by_legislator_id.insert(i, element)
 
 
+with open(f"{cwd}/source" + "\\bills.csv", newline="", encoding="utf-8") as csv_bills:
+    reader = csv.reader(csv_bills)
+    headers = next(reader)
+    for index, element in enumerate(reader):
+        bills.insert(i, element)
+
+with open(f"{cwd}/source" + "\\votes.csv", newline="", encoding="utf-8") as csv_votes:
+    reader = csv.reader(csv_votes)
+    headers = next(reader)
+    for index, element in enumerate(reader):
+        votes.insert(i, element)
+        map_bill_to_vote[element[1]] = element[0]
+
+# writing
+
 with open(
     cwd + "\\legislator_support_oppose_count.csv", "w", newline="", encoding="utf-8"
 ) as file:
@@ -101,6 +140,41 @@ with open(
             legislator[1],
             support_count,
             oppose_count,
+        ]
+        writer.writerow(row)
+
+with open(cwd + "\\bill_support.csv", "w", newline="", encoding="utf-8") as file:
+    writer = csv.writer(file)
+    fields = ["id", "title", "supporter_count", "opposer_count", "primary_sponsor"]
+
+    writer.writerow(fields)
+    i = 0
+    vote_count = {}
+    for index, vote_result in enumerate(sorted_votes_by_legislator_id):
+        key = vote_result[2]
+        if key not in vote_count:
+            vote_count[key] = [0, 0]
+
+        if int(vote_result[3]) == 1:
+            vote_count[key][0] += 1
+        else:
+            vote_count[key][1] += 1
+
+    for index, bill in enumerate(bills):
+        sponsor_index = search_legislator_by_id(sorted_legislators, bill[2])
+        sponsor = ""
+        if sponsor_index != -1:
+            sponsor = sorted_legislators[sponsor_index][1]
+        else:
+            sponsor = "Unkown"
+
+        vote_id = map_bill_to_vote[bill[0]]
+        row = [
+            bill[0],
+            bill[1],
+            vote_count[vote_id][0],
+            vote_count[vote_id][1],
+            sponsor,
         ]
         writer.writerow(row)
 
