df80df7474f3874480aaaada772f4ba8ff70ce1d
Francisco Andrade <francisco.andrade@tolife.com.br>
2023-10-30 13:51:52 -0300

    adding key to search and sort

--- writers.py
+++ writers.py
@@ -76,12 +76,12 @@ def write_bill_support(
                 vote_count[key][1] += 1
 
         for _, bill in enumerate(bills):
-            sponsor_index = search_legislator_by_id(sorted_legislators, bill[2])
+            sponsor_index = search_legislator_by_id(sorted_legislators, bill[2], key=0)
             sponsor = ""
             if sponsor_index != -1:
                 sponsor = sorted_legislators[sponsor_index][1]
             else:
-                sponsor = "Unkown"
+                sponsor = "Unknown"
 
             vote_id = map_bill_to_vote[bill[0]]
             row = [
@@ -94,17 +94,17 @@ def write_bill_support(
             writer.writerow(row)
 
 
-def search_legislator_by_id(input_array: Sequence, target: int) -> int:
+def search_legislator_by_id(input_array: Sequence, target: int, key) -> int:
     """Find legislator by id, assuming a sorted input sequence"""
     start = 0
     end = len(input_array) - 1
     while start <= end:
         target_index = (start + end) // 2
 
-        if input_array[target_index][0] == target:
+        if input_array[target_index][key] == target:
             return target_index
 
-        elif input_array[target_index][0] > target:
+        if input_array[target_index][key] > target:
             end = target_index - 1
         else:
             start = target_index + 1
